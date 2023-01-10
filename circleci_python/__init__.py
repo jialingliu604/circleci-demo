fd09a30a0919e60aeffee18729a93e5cfaf74f60
jialingliu604 <jialing.liu@thoughtworks.com>
2023-01-10 23:49:40 +0800

    add test

--- circleci_python/__init__.py
+++ circleci_python/__init__.py
@@ -1,2 +1,1 @@
-
-
+from circleci_python.main import Add
---
+++ circleci_test/main_test.py
@@ -0,0 +1,13 @@
+# sys.path.append("")
+from circleci_python.main import Add
+
+
+
+def TestAdd():
+    assert Add(2, 3) == 5
+    print("Add Function works correctly")
+
+
+if __name__ == '__main__':
+    TestAdd()
+    # print(sys.path)

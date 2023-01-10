ec7b323ae43062da45dc75324450da3b0e36084f
jialingliu604 <jialing.liu@thoughtworks.com>
2023-01-11 01:08:32 +0800

    should build before test

--- circleci_test/main_test.py
+++ circleci_test/main_test.py
@@ -1,8 +1,9 @@
 # sys.path.append("")
+import os
+import sys
 from ..circleci_python.main import Add
 
-
-
+sys.path.append(os.path.join("/home/circleci/circleci-demo"))
 def TestAdd():
     assert Add(2, 3) == 5
     print("Add Function works correctly")
